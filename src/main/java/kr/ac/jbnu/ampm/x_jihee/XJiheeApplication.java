2f2a4a66bb8b9f631a0a363b4d344bb9ec1c6e21
jihee-daily <kim76955@gmail.com>
2021-04-09 20:13:45 +0900

    first Upload to the Repository

---
+++ src/main/java/kr/ac/jbnu/ampm/x_jihee/XJiheeApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.x_jihee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XJiheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XJiheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/ampm/x_jihee/XJiheeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.x_jihee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XJiheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
